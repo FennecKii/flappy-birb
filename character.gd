e89fe47ec8aaabb74b3d628c89ececd8291f6d57
FennecKii <83555681+FennecKii@users.noreply.github.com>
2025-03-14 09:27:16 -0600

    Character Movement
    Added stationary jumping character movement

---
+++ character.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
