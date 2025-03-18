db4c2f8bb233e3d228c39e2d4a597ae38e209a30
Andrew Liu <andrewliu101720@gmail.com>
2025-03-18 16:37:29 -0600

    Global script
    - Added and configured global script

---
+++ global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const JUMP_VELOCITY: float = -250.0
+const TUBE_SPEED: float = 100.0
+
+signal thing_killed
+
+var score: int = 0
+var highscore: int = 0
+
+func initialize_game_state():
+	score = 0
