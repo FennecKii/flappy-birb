a0f2ee150879b232f9e04cf0c362245da82b3786
Andrew Liu <andrewliu101720@gmail.com>
2025-03-18 16:38:00 -0600

    Death panel
    - Added death panel with functionality to display score, highscore and play again

---
+++ death_panel.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var score_label: Label = $Panel/VBoxContainer/Score
+@onready var highscore_label: Label = $Panel/VBoxContainer/Highscore
+
+func _on_play_again_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_visibility_changed():
+	if self.visible == true:
+		score_label.text = "Score: " + str(Global.score)
+		highscore_label.text = "Highscore: " + str(Global.highscore)
