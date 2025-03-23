4b692dbc7ff4eedfde1cbd5f09d58b16e828eaba
Andrew Liu <andrewliu101720@gmail.com>
2025-03-23 14:06:02 -0600

    Random Colors to leader-board

--- addons/silent_wolf/Scores/score_item.gd
+++ addons/silent_wolf/Scores/score_item.gd
@@ -1,6 +1,12 @@
 extends Panel
 
 @onready var thing: CharacterBody2D = $Thing
+@onready var thing_sprite: AnimatedSprite2D = $Thing/Thing
 
 func _ready():
 	thing.set_physics_process(false)
+	thing.set_process(false)
+	thing_sprite.material.set("shader_parameter/outline_color", Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255), 255))
+	thing_sprite.material.set("shader_parameter/body_color", Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255), 255))
+	thing_sprite.material.set("shader_parameter/mouth_color", Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255), 255))
+	thing_sprite.material.set("shader_parameter/eye_color", Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255), 255))
